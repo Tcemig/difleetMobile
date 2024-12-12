44cea823b57ee251cc0ee5e8199a95d808deb875
Tyler Emig <tcemig@gmail.com>
2024-12-11 16:56:37 -0800

    version 1.0.1

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 // Testing
-// const NGROK_URL = 'https://2698-108-89-75-186.ngrok-free.app';
+const NGROK_URL = 'https://63eb-108-89-75-186.ngrok-free.app';
 
 // Production
-const NGROK_URL = 'https://www.difleet.com';
+// const NGROK_URL = 'https://www.difleet.com';
 
 export default NGROK_URL;
