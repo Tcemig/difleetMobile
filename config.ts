a698855de08b250a04f11242b01bd41e7fbd5578
Tcemig3 <123010169+Tcemig3@users.noreply.github.com>
2024-12-12 09:31:20 -0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 // Testing
-const NGROK_URL = 'https://c212-108-89-75-186.ngrok-free.app';
+// const NGROK_URL = 'https://c212-108-89-75-186.ngrok-free.app';
 
 // Production
-// const NGROK_URL = 'https://www.difleet.com';
+const NGROK_URL = 'https://www.difleet.com';
 
 export default NGROK_URL;
